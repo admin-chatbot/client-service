fc0b98d1b9be7a9ce26fad7505f9b278beba9b95
Dharam <patwari.dharmendra@gmail.com>
2024-01-21 17:07:50 +0530

    Changes for Search

--- src/main/java/com/voicebot/commondcenter/clientservice/service/ServiceService.java
+++ src/main/java/com/voicebot/commondcenter/clientservice/service/ServiceService.java
@@ -1,5 +1,6 @@
 package com.voicebot.commondcenter.clientservice.service;
 
+import com.voicebot.commondcenter.clientservice.dto.ServiceSearchRequest;
 import com.voicebot.commondcenter.clientservice.entity.Application;
 import com.voicebot.commondcenter.clientservice.entity.Service;
 import org.bson.types.ObjectId;
@@ -23,4 +24,6 @@ public interface ServiceService {
     List<Service> findAllByApplicationId(Long applicationId);
 
     List<Service> findServiceByClientIdAndKeywordLike(Long clientId,String keyword);
+
+    public List<Service> search(ServiceSearchRequest serviceSearchRequest);
 }
