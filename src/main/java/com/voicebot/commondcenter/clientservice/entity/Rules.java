0168f2c56e311642f3029e79920b99d7e80f0390
Dharam <patwari.dharmendra@gmail.com>
2024-03-14 23:32:40 +0530

    Added entities for rule, step, story

---
+++ src/main/java/com/voicebot/commondcenter/clientservice/entity/Rules.java
@@ -0,0 +1,29 @@
+package com.voicebot.commondcenter.clientservice.entity;
+
+import com.voicebot.commondcenter.clientservice.entity.AbstractBaseEntity;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+
+@Document(collection = "rules")
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Rules extends AbstractBaseEntity implements Serializable {
+
+    @Id
+    private Long id;
+
+    @NotBlank(message = "Rule Name should not null or empty.")
+    private String rulename;
+
+    @NotBlank(message = "Step id should not be null or empty.")
+    private Long stepId;
+
+}
